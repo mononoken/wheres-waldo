6b39c9c3db297af1b25fe996e57b4cf3dac7c132
mononoken <ken.maeshima@pm.me>
2024-08-06 13:53:26 -0700

    Init rails project with vite_rails, inertia_rails, and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/frontend/**/*.{js,jsx,ts,tsx,svelte,vue}",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
